ad7728ac5c6db823dd6edb7f9fb180edd2238581
Kapil Gwal <2909kapil2001@gmail.com>
2024-09-11 23:17:19 +0530

    connecting to the socket io package

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const socket = require('socket.io');
 const http = require('http');
-const {Chess} = require('chess.js');
+const { Chess } = require('chess.js');
 const { log } = require('console');
 const path = require("path");
 
@@ -11,19 +11,64 @@ const io = socket(server);
 const chess = new Chess();
 
 let players = {};
-let currentPlayer = "W";
+let currentPlayer = "w";
 
-app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req,res) => {
-    res.render("index",{title : "Chess Game"});
+app.get("/", (req, res) => {
+    res.render("index", { title: "Chess Game" });
 });
 
-io.on("connection",function(uniquesocket){
+io.on("connection", function (uniquesocket) {
     console.log("connected");
-})
 
-server.listen(3000, function(){
+    if (!players.white) {
+        players.white = uniquesocket.id;
+        uniquesocket.emit("playerRole", "w");
+    }
+
+    else if (!players.black) {
+        players.black = uniquesocket.id;
+        uniquesocket.emit("playerRole", "b");
+    }
+
+    else {
+        uniquesocket.emit("spectetorRole");
+    }
+
+    uniquesocket.on("disconnect", function () {
+        if (uniquesocket.id === players.white) {
+            delete players.white;
+        } 
+        else if (uniquesocket.id === players.black) {
+            delete players.black;
+        }
+    });
+
+    uniquesocket.on("move", (move) => {
+        try{
+
+            if(chess.turn() === 'w' && socket.id !== players.white){
+                return;
+            }
+            if(chess.turn() === 'b' && socket.id !== players.black){
+                return;
+            }
+
+            const result = chess.move(move);
+            if(result){
+                currentPlayer = chess.turn();
+            }
+
+        }
+        catch(err){
+
+        }
+    })
+
+});
+
+server.listen(3000, function () {
     console.log("Listen the server on port 3000");
 })
--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,1 +1,6 @@
 const socket = io();
+
+socket.emit("Hello");
+socket.on("Nothing", function(){
+    console.log("Nothing Recieved");
+});
